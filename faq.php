3cb19b66c8321bd14726416e3e2a61db01234bad
IrinaSoria <50781299+IrinaSoria@users.noreply.github.com>
2019-08-15 18:46:58 -0300

    Add files via upload

---
+++ faq.php
@@ -0,0 +1,75 @@
+<?php
+ ?>
+ <!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <style>
+@import url('https://fonts.googleapis.com/css?family=Crete+Round&display=swap');
+</style>
+    <link rel="stylesheet" href="book.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <title>Preguntas Frecuentes</title>
+  </head>
+  <header>
+    <?php
+    include("navBar.php");
+    ?>
+
+  </header>
+  <body>
+    <div class"container">
+
+    <h1>Preguntas Frecuentes</h1>
+
+
+      <div>
+          <h3>¿Cuáles son los medios de pago aceptados?
+
+          </h3>
+              <p>Los medios de pago aceptados son:</p>
+
+                <p>-Tarjetas de Débito y Credito Visa o Mastercard</p>
+
+                <p>-Pago mis cuentas</p>
+              </div>
+
+        <div>
+            <h3>¿Cuánto cuesta el envío?
+            </h3>
+            <p>El envio se calcula segun la cantidad de kilometros. En el momento que pidas el producto se te mostrara un estimado del costo.</p>
+
+          </div>
+        <div>
+            <h3>¿En cuánto tiempo recibo mi pedido?
+
+            </h3>
+            <p>El tiempo maximo estimado es de 10 días dependiendo de la localidad.  En caso que desee recir el pedido en una menor cantidad de tiempo debera abonar una tarifa diferente </p>
+          </div>
+
+        <div>
+            <h3>Me gustaría enviar un regalo, ¿es posible?
+
+            </h3>
+            <p>Por supuesto! no pierdas la oportuniad de hacer el mejor regalo. Inicanos en la direcion de envio donde deseas que llegue e indica que deseas recibir la factura a tu email.</p>
+          </div>
+
+    <div>
+          <h3> No encuentro la respuesta a mi pregunta, ¿qué hago?
+
+          </h3>
+          <p>Haga clic en la página de "Contactos" y envíenos su pregunta, le responderemos tan pronto como sea posible.</p>
+
+    </div>
+
+    </div>
+
+        <footer>
+            <?php
+            include("footer.php");
+             ?>
+      </footer>
+
+      </body>
+</html>
